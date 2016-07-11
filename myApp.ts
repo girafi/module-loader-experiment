9bc822ab93e1b902dd1463baf571c39d13c8a730
girafi <girafi>
2016-07-11 22:46:59 +0200

    added typescript and typings

--- myApp.ts
+++ myApp.ts
@@ -1,9 +1,11 @@
 (function () {
+
     var module = angular.module('myModule', []);
+
     module.controller('myController', ['$scope',
         function ($scope) {
             $scope.myVar = 'gugus';
         }
-    ]);
+    ])
+
 })();
-//# sourceMappingURL=myApp.js.map
