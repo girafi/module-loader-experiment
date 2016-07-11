0c601a716df113ee88cb4a4f852fec4744b66c8e
girafi <girafi>
2016-07-11 23:00:45 +0200

    cleanup

--- myApp.js
+++
@@ -1,9 +0,0 @@
-(function () {
-    var module = angular.module('myModule', []);
-    module.controller('myController', ['$scope',
-        function ($scope) {
-            $scope.myVar = 'gugus';
-        }
-    ]);
-})();
-//# sourceMappingURL=myApp.js.map
